e6e047a4719013722fd459c895220b87cb861958
Darlan Voss <darlanvoss@gmail.com>
2022-08-04 16:01:33 -0300

    Client and Controls View

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <BlockGame />
+  </div>
+</template>
+<script>
+import BlockGame from "./components/BlockGame.vue";
+
+export default {
+  name: "app",
+  components: {
+    BlockGame,
+  },
+};
+</script>
+
+<style></style>
